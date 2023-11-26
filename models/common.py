d84d4e006233b0a980f4001a576097f8e854a2d7
o-ibrahim33 <omar.ibrahim@proemion.com>
2023-11-26 22:14:48 +0100

    Export Model to ONNX

--- models/common.py
+++ models/common.py
@@ -100,7 +100,7 @@ class Conv(nn.Module):
     
 class QuantConv(nn.Module):
     # Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)
-    default_act = QuantSiLU()  # default activation
+    
 
     def __init__(self, c1, c2, k=1, s=1, p=None, weight_bit_width=4, act_bit_width=4, g=1, d=1, act=True):
         super().__init__()
@@ -114,6 +114,12 @@ class QuantConv(nn.Module):
             dilation=d,
             bias=False
         )
+        
+        self.default_act = QuantReLU(
+            per_channel_broadcastable_shape=(1, c2, 1, 1),
+            scaling_per_channel=False
+        ) 
+        
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else QuantIdentity()
 
